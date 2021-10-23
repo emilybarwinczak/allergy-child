f1f109f65a912396fbf690935c5f753272b07038
Emily Barwinczak <ebarw@macbook-air.home>
2021-10-23 12:39:44 -0400

    third commit

---
+++ js/app.js
@@ -0,0 +1,5 @@
+// we need to get our canvas, save it to a variable, so we can access it(and utilize it)
+const game = document.getElementById('canvas')
+// another thing we'll do here, is get the movement tracker
+const moveDisplay = document.getElementById('movement')
+
