64db35f77f90342a6cd38586f7ce7c0faafe6d03
Emily Barwinczak <ebarw@macbook-air.home>
2021-10-26 17:02:46 -0400

    fifth commit

--- js/app.js
+++ js/app.js
@@ -50,11 +50,54 @@ function Crawler(x, y, color, width, height) {
     }
 }
 
-// let player = new Crawler(50, 630, 'blue', 16, 16)
-let player = new Crawler(50, 130, 'blue', 16, 16)
-let friendshouse = new Crawler(750, 150, 'black', 48, 32)
+let player = new Crawler(50, 630, 'white', 18, 18)
+let friendshouse = new Crawler(750, 150, 'white', 48, 32)
 console.log('this is the player', player)
 console.log('this is the friends house', friendshouse)
+
+// add new crawlers for cats? with some sort of function that pushes them to move at random times?
+
+
+// function add_img() {
+//     const img = document.createElement('img');
+//     img.src = 'https://www.pngitem.com/pimgs/m/149-1499086_transparent-flying-cat-png-flying-cat-no-background.png';
+//     document.getElementById('body').appendChild(img);
+// }
+
+
+let catsbrown = new Crawler(Math.floor(Math.random() * game.width), Math.floor(Math.random() * game.height), 'brown', 15, 15)
+let catsorange = new Crawler(Math.floor(Math.random() * game.width), Math.floor(Math.random() * game.height), 'orange', 15, 15)
+let catsblack = new Crawler(Math.floor(Math.random() * game.width), Math.floor(Math.random() * game.height), 'black', 15, 15)
+//can i simplify this into an array? and make them all move randomly as well?
+
+
+
+
+
+// let cats = new spawnObjects('brown', 15, 15)
+// console.log(cats)
+// function spawnObjects(color, width, height) {
+//     x = Math.floor(Math.random() * game.width)
+//     y = Math.floor(Math.random() * game.height)
+//     this.color = color
+//     this.width = width
+//     this.height = height
+    
+//     this.render = function () {
+//         ctx.fillStyle = this.color
+//         ctx.fillRect(x, y, this.width, this.height)
+//     }
+// }
+
+
+
+// function move() {
+//     const querySelector(cats);
+
+// }
+// setInterval() => {
+//     //want the object to move around here?
+// }
 
 // Here we're going to set up our movement handler
 // the movementhandler will be an event listener
@@ -122,14 +165,22 @@ const gameLoop = () => {
     moveDisplay.innerText = `X: ${player.x}\nY: ${player.y}`
     // check if the endingPoint has been reached, if not, render the endingPoint
     friendshouse.render()
+    // cats.render(
+    //     // function image (){
+    //     //     img.src = 'https://www.pngitem.com/pimgs/m/149-1499086_transparent-flying-cat-png-flying-cat-no-background.png';
+    //     //     document.body.appendChild(img);
+    //     // }
+    // )
+    catsbrown.render()
+    catsorange.render()
+    catsblack.render()
+    player.render()
     detectHit()
     if (friendshouse.reached) {
         stopGameLoop()
         // add in our detection to see if the hit has been made
         console.log('You win!')
     }
-    // render our player
-    player.render()
 }
 
 // we also need to declare a function that will stop our animation loop
@@ -138,3 +189,229 @@ const gameLoop = () => {
 document.addEventListener('keydown', movementHandler)
 // the timing function will determine how and when our game animates
 let gameInterval = setInterval(gameLoop, 70)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+// const gw = game.width;
+// const gh = game.height;
+
+// const cats = [];
+// for (i = 0; i < 3; i++) {
+//     newCat();
+// }
+// drawAll();
+
+// const continueAnimating = true;
+// const lastTime = performance.now();
+// requestAnimationFrame(animate);
+// function newCat() {
+//     let r = 10 + Math.random() * 15;
+//     let x = Math.random() * (gw-2*r) + r;
+//     let y = Math.random() * (gh-2*r) + r;
+//     while (true) {
+//         let hit = 0;
+//         for (i = 0; i < cats.length; i++){
+//             const cat = cats[i];
+//             let dx = x - cat.x;
+//             let dy = y - cat.y;
+//             let rr = r + cat.radius;
+//             if (dx * dx + dy * dy <rr * rr) {
+//                 hit++;
+//             }
+//         }
+//         if (hit == 0) {
+//             break;
+//         }
+//     let x = Math.random() * (gw-2*r) + r;
+//     let y = Math.random() *(gh-2*r) + r;
+//     }
+//     cats.push({
+//         x: x,
+//         y: y,
+//         radius: r,
+//         color: randomColor(),
+//         countdown: 500 + Math.random() * 1000
+//     });
+// }
+// function animate(time) {
+//     if (continueAnimating) {
+//         requestAnimationFrame(animate);
+//     }
+//     var elapsed = time - lastTime;
+//     lastTime = time;
+//     var i = cats.length;
+//     while (--i >= 0) {
+//         var cat = cats[i];
+//         cat.countdown -= elapsed;
+//         if (cat.countdown < 0) {
+//             cats.splice(i, 1);
+//             newCat();
+//         }
+//     }
+//     // draw circles
+//     drawAll();
+// }
+
+// function drawAll() {
+//     ctx.clearRect(0, 0, cw, ch);
+//     for (var i = 0; i < cats.length; i++) {
+//         var cat = cats[i];
+//         ctx.beginPath();
+//         ctx.arc(cat.x, cat.y, cat.radius, 0, Math.PI * 2);
+//         ctx.closePath();
+//         ctx.fillStyle = cat.color;
+//         ctx.fill();
+//     }
+// }
+// function randomColor() {
+//     return ('#' + Math.floor(Math.random() * 16777215).toString(16));
+// }
+// $("#stop").click(function () {
+//     continueAnimating = false;
+// });
+
+
+//////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// function cats() {
+//     this.x = Math.random() * (game.width - (game.width - game.width)) + (game.width - game.width);
+//     this.y = 0;
+//     this.vx = 5.5;
+//     this.vy = 1;
+//     this.radius = 5;
+//     this.color = 'brown';
+//     this.gravity = 1;
+//     this.gravitySpeed = 0;
+//     this.bounce = 1;
+//     this.ctx = game.getContext('2d');
+//     this.draw = function() {
+//       this.ctx.beginPath();
+//       this.ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+//       this.ctx.closePath();
+//       this.ctx.fillStyle = this.color;
+//       this.ctx.fill();
+//     }
+//     this.newPos = function() {
+//       this.gravitySpeed += this.gravity;
+//       this.x += this.vx;
+//       this.y += this.vy + this.gravitySpeed;
+//       this.hitBottom();
+//     }
+//     this.hitBottom = function() {
+//       var rockbottom = game.height - this.radius;
+//       if (this.y > rockbottom) {
+//         this.y = rockbottom;
+//         this.gravitySpeed = -(this.gravitySpeed / 1.5 * this.bounce);
+//       }
+//     }
+//   }
+  
+//   var ball = new cats();
+
+
+//   function drawf() {
+//     // ball.ctx.clearRect(0, 0, game.width, game.height);
+//     ball.draw();
+//     // console.log(ball);
+//     ball.newPos();
+//     raf = window.requestAnimationFrame(drawf);
+// }
+
+// // setInterval(drawf, 35000)
+//   game.addEventListener('keypress', function(e) {
+//     raf = window.requestAnimationFrame(drawf);
+//   });
+  
+//   ball.draw();
+
+//////////////////////////////////////////////////////////////////////////////////////////////
