43eff3e297a0423e83a6ef382a6467bdec4ada56
Emily Barwinczak <ebarw@MacBook-Air.local>
2021-12-22 14:55:57 -0500

    working out functionality kinks

--- js/app.js
+++ js/app.js
@@ -1,37 +1,96 @@
-// we need to get our canvas, save it to a variable, so we can access it(and utilize it)
-const game = document.getElementById('canvas')
-// another thing we'll do here, is get the movement tracker
-const moveDisplay = document.getElementById('movement')
+const game = document.getElementById("canvas")
+const ctx = game.getContext("2d")
+const gameName = document.getElementById("gameName")
 
-// we're setting up height and width variables BASED ON computed style
-// that means we're using setAttribute in conjunction with getComputedStyle
+// const start = document.querySelector(".start")
+const startGame = document.getElementById("startGame")
+const gameOver = document.getElementById("gameOver")
+const gameWon = document.getElementById("gameWon")
+
+const startButton = document.getElementById("startButton")
+const resetButton = document.getElementById("resetButton")
+const moveDisplay = document.getElementById("movement")
+
 game.setAttribute('width', getComputedStyle(game)['width'])
 game.setAttribute('height', getComputedStyle(game)['height'])
 
-// now we need to get the game's context so we can add to it, draw on it, create animations etc
-// we do this with the built in canvas method, getContext
-const ctx = game.getContext('2d')
-
 const inhalers = document.querySelectorAll(".inhaler")
-console.log('this should be an inhaler', inhalers)
-///commenting out below FOR NOW only
-
-// window.alert("Welcome to Allergy Child! To play, you can use the following keys: W = Up S = Down A = Left D = Right")
+// console.log('this should be an inhaler', inhalers)
 
 
+// Game Menus
 
-let catDirection = true 
+startButton.addEventListener("click", () => {
+    startGame.style.display = "none"
+    game.style.display = "block"
+    gameOver.style.display = "none"
+    gameWon.style.display = "none"
+    gameName.style.display = "none"
+})
 
-function catsbrownDirectionX () {
-    } 
-function catbrownMoveX () {
-    if (catsbrown.x > 1050){
+function gameHasEnded() {
+    stopGameLoop()
+    startGame.style.display = "none"
+    game.style.display = "none"
+    gameOver.style.display = "block"
+    gameWon.style.display = "none"
+    gameName.style.display = "none"
+    resetButton.addEventListener("click", () => {
+        reset()
+    })
+}
+
+function gameIsWon() {
+    stopGameLoop()
+    startGame.style.display = "none"
+    game.style.display = "none"
+    gameOver.style.display = "none"
+    gameWon.style.display = "block"
+    gameName.style.display = "none"
+    resetButton.addEventListener("click", () => {
+        reset()
+    })
+}
+
+function reset() {
+    location.reload()
+}
+
+
+
+
+
+// const button = document.getElementById("button");
+// button.addEventListener("click", Start);
+// function Start() {
+//     console.log("Started");
+//     button.removeEventListener("click", Start);
+//     button.addEventListener("click", Stop);
+//     button.value = "Stop";
+// }
+// function Stop(){
+//     console.log("Stopped");
+//     button.removeEventListener("click", Stop);
+//     button.addEventListener("click", Start);
+//     button.value = "Start";
+// }
+
+
+
+
+
+let catDirection = true
+
+function catsbrownDirectionX() {
+}
+function catbrownMoveX() {
+    if (catsbrown.x > 1050) {
         catDirection = false
     } else if (catsbrown.x < 0) {
         catDirection = true
     }
     // console.log('Cat Direction' + catDirection)
-    if (catDirection === true){
+    if (catDirection === true) {
         catsbrown.x += 8
         // console.log('+')
     } else {
@@ -40,16 +99,16 @@ function catbrownMoveX () {
     }
 }
 
-function catsorangeDirectionX () {
-    } 
-function catorangeMoveX () {
-    if (catsorange.x > 1050){
+function catsorangeDirectionX() {
+}
+function catorangeMoveX() {
+    if (catsorange.x > 1050) {
         catDirection = false
     } else if (catsorange.x < 0) {
         catDirection = true
     }
     // console.log('Cat Direction' + catDirection)
-    if (catDirection === true){
+    if (catDirection === true) {
         catsorange.x += 7
         // console.log('+')
     } else {
@@ -58,16 +117,16 @@ function catorangeMoveX () {
     }
 }
 
-function catsblackDirectionY () {
-} 
-function catblackMoveY () {
-    if (catsblack.y > 650){
+function catsblackDirectionY() {
+}
+function catblackMoveY() {
+    if (catsblack.y > 650) {
         catDirection = false
     } else if (catsblack.y < 0) {
         catDirection = true
     }
     // console.log('Cat Direction' + catDirection)
-    if (catDirection === true){
+    if (catDirection === true) {
         catsblack.y += 7
         // console.log('+')
     } else {
@@ -79,13 +138,13 @@ function catblackMoveY () {
 
 
 const crazyImg = new Image()
-crazyImg.src = ('https://i.imgur.com/47ArcEv.png')
+crazyImg.src = ('../js/imgs/crazycat.png')
 
 const flyingImg = new Image()
-flyingImg.src = ('https://i.imgur.com/9sPV5ma.png')
+flyingImg.src = ('../js/imgs/flyingcat.png')
 
 const sittingImg = new Image()
-sittingImg.src = ('https://i.imgur.com/QHmkIki.png')
+sittingImg.src = ('../js/imgs/sittingcat.png')
 
 const ellieImg = new Image()
 ellieImg.src = ('https://i.imgur.com/ZhVva00.png')
@@ -102,20 +161,33 @@ function Crawler(url, x, y, width, height) {
     this.height = height
     this.reached = false
     this.render = function () {
-        ctx.drawImage(this.url, this.x, this.y, this.width, this.height)
+        // ctx.drawImage(this.url, this.x, this.y, this.width, this.height)
     }
 }
 
-let catsbrown = new Crawler (crazyImg, Math.floor(Math.random() * game.width), Math.floor(Math.random() * game.height), 90, 100)
-let catsorange = new Crawler (flyingImg, Math.floor(Math.random() * game.width), Math.floor(Math.random() * game.height), 80, 80)
-let catsblack = new Crawler (sittingImg, Math.floor(Math.random() * game.width), Math.floor(Math.random() * game.height), 80, 80)
+let catsbrown = new Crawler(crazyImg, Math.floor(Math.random() * game.width), Math.floor(Math.random() * game.height), 90, 100)
+let catsorange = new Crawler(flyingImg, Math.floor(Math.random() * game.width), Math.floor(Math.random() * game.height), 80, 80)
+let catsblack = new Crawler(sittingImg, Math.floor(Math.random() * game.width), Math.floor(Math.random() * game.height), 80, 80)
 
-let player = new Crawler (ellieImg, 0, 470, 80, 80)
-let friendshouse = new Crawler(houseImg, 850, 0, 110, 75)
+
+
+function Players(url, x, y, width, height) {
+    this.url = url
+    this.x = x
+    this.y = y
+    this.width = width
+    this.height = height
+    this.reached = false
+    this.render = function () {
+        // ctx.drawImage(this.url, this.x, this.y, this.width, this.height)
+    }
+}
+let player = new Players(ellieImg, 0, 470, 80, 80)
+let friendshouse = new Players(houseImg, 850, 0, 110, 75)
 
 
 let movementHandler = (e) => {
-    switch(e.key.toLowerCase()) {
+    switch (e.key.toLowerCase()) {
         case ('w'):
             // move up
             player.y -= 10
@@ -148,17 +220,18 @@ let movementHandler = (e) => {
 }
 
 // let counter = 2
-function removeInhaler () {
+function removeInhaler() {
     for (let i = 0; i < 3; i++) {
-        
-        if(inhalers[i].style.display = "none"){
+
+        if (inhalers[i].style.display = "none") {
             break;
         }
         else {
             inhalers[i].style.display = "none"
         }
-    }    
-        
+    }
+
+
     //     function removeInhaler () {
     //     // console.log(inhalers)
     // for (let i = 0; i < inhalers.length; i++) {
@@ -197,8 +270,8 @@ const detectHit = () => {
         player.y + player.height > catsbrown.y
     ) {
         catsbrown.reached = true
-        removeInhaler()
-        document.querySelector('#bottom-right > h2').innerText = 'You lose a life!'
+        // removeInhaler()
+        // document.querySelector('#bottom-right > h2').innerText = 'You lose a life!'
     } else if (
         player.x < catsorange.x + catsorange.width &&
         player.x + player.width > catsorange.x &&
@@ -206,26 +279,56 @@ const detectHit = () => {
         player.y + player.height > catsorange.y
     ) {
         catsorange.reached = true
-        removeInhaler()
-        document.querySelector('#bottom-right > h2').innerText = 'You lose a life!'
-        } else if (
+        // removeInhaler()
+        // document.querySelector('#bottom-right > h2').innerText = 'You lose a life!'
+    } else if (
         player.x < catsblack.x + catsblack.width &&
         player.x + player.width > catsblack.x &&
         player.y < catsblack.y + catsblack.height &&
         player.y + player.height > catsblack.y
     ) {
         catsblack.reached = true
-        removeInhaler()
+        // removeInhaler()
         // counter--
-        document.querySelector('#bottom-right > h2').innerText = 'You lose a life!'
+        // document.querySelector('#bottom-right > h2').innerText = 'You lose a life!'
+    // } else if (
+    //     player.x < Crawler.x + Crawler.width &&
+    //     player.x + player.width > Crawler.x &&
+    //     player.y < Crawler.y + Crawler.height &&
+    //     player.y + player.height > Crawler.y
+    // ) {
+    //     Crawler.reached = true
+    //     removeInhaler()
+    //     // counter--
+    //     document.querySelector('#bottom-right > h2').innerText = 'You lose a life!'
+    }
+}
+
+
+// tracks detected hits
+let collision = 0
+const catCollision = () => {
+    return collision += 1
+}
+
+
+// end of game logic
+function gameEnds() {
+    if (collision >= 3) {
+        gameHasEnded()
+    }
+}
+
+function wonGame() {
+    if (collision <= 3 && friendshouse.reached == true) {
+        gameIsWon()
     }
 }
 
 
 
-let stopGameLoop = () => {clearInterval(gameInterval)}
-// we're going to set up our game loop, to be used in our timing function
-// set up gameLoop function, declaring what happens when our game is running
+
+let stopGameLoop = () => { clearInterval(gameInterval) }
 
 const gameLoop = () => {
     // clear the canvas
